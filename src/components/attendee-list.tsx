211bd1c72eb9acd9460a91c8b3924d40445ae670
Bruna G de Jesus <bgdj17@gmail.com>
2024-04-03 19:07:41 -0300

    add url stage

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -7,35 +7,85 @@ import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
-import { attendees } from '../data/attendees'
-import { ChangeEvent, useState } from 'react'
+// import { attendees } from '../data/attendees'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
+interface Attendee {
+    id: string
+    name: string
+    email: string
+    createdAt: string
+    checkedInAt: string | null
+}
+
 export function AttendeeList() {
-    const [search, setSearch] = useState('')
-    const [page, setPage] = useState(1)
+    const [search, setSearch] = useState(() => {
+        const url = new URL(window.location.toString())
+        if (url.searchParams.has('search')) {
+            return url.searchParams.get('search') ?? ''
+        }
+        return ''
+    })
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString())
+        if (url.searchParams.has('page')) {
+            return Number(url.searchParams.get('page'))
+        }
+        return 1
+    })
+    const [total, setTotal] = useState(0)
+    const [attendees, setAttendees] = useState<Attendee[]>([])
 
-    const totalPages = Math.ceil(attendees.length /10)
+    useEffect(() => {
+        const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+        url.searchParams.set('pageIndex', String(page - 1))
+        search.length > 0 && url.searchParams.set('query', search)
 
-    function onSearchInputChange(event: ChangeEvent<HTMLInputElement>){
-        setSearch(event.target.value)
-    }
-    
-    function goToNextPage(){
-        setPage(page + 1)
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setAttendees(data.attendees)
+                setTotal(data.total)
+            })
+    }, [page, search])
+
+    const totalPages = Math.ceil(total / 10)
+
+    function onSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
+        setCurrentSearch(event.target.value)
+        setCurrentPage(1)
     }
 
-    function goToPreviousPage(){
-        setPage(page - 1)
+    function goToNextPage() {
+        setCurrentPage(page + 1)
     }
 
-    function goToFirstPage(){
-        setPage(1)
+    function goToPreviousPage() {
+        setCurrentPage(page - 1)
     }
-    function goToLastPage(){
-        setPage(totalPages)
+
+    function goToFirstPage() {
+        setCurrentPage(1)
+    }
+    function goToLastPage() {
+        setCurrentPage(totalPages)
+    }
+
+    function setCurrentPage(page: number) {
+        const url = new URL(window.location.toString())
+        url.searchParams.set('page', String(page))
+        window.history.pushState({}, '', url)
+        setPage(page)
+    }
+
+    function setCurrentSearch(search: string) {
+        const url = new URL(window.location.toString())
+        url.searchParams.set('search', String(search))
+        window.history.pushState({}, '', url)
+        setSearch(search)
     }
 
     return (
@@ -44,8 +94,14 @@ export function AttendeeList() {
                 <h1 className="text-2xl font-bold"> Participante </h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className='size-4 text-emerald-300' />
-                    <input onChange={onSearchInputChange} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" type="text" placeholder="Buscar participante..." />
-                </div>{search}
+                    <input
+                        onChange={onSearchInputChange}
+                        className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+                        type="text"
+                        placeholder="Buscar participante..."
+                        value={search}
+                    />
+                </div>
             </div>
 
             <Table>
@@ -62,7 +118,7 @@ export function AttendeeList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {attendees.slice((page - 1)* 10, page * 10 ).map(attendee => {
+                    {attendees.map(attendee => {
                         return (
                             <TableRow className='border-b border-white/10 hover:bg-white/5' key={attendee.id}>
                                 <TableCell className='py-3 px-4 text-left text-sm text-zinc-300'>
@@ -76,7 +132,12 @@ export function AttendeeList() {
                                     </div>
                                 </TableCell >
                                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell >
-                                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell >
+                                <TableCell>{
+                                    attendee.checkedInAt == null
+                                        ? <span className='text-zinc-400'>Não fez Check-in</span>
+                                        : dayjs().to(attendee.checkedInAt)
+                                }
+                                </TableCell >
                                 <TableCell>
                                     <IconButton transparent ><MoreHorizontal className='size-4' /></IconButton>
                                 </TableCell >
@@ -86,7 +147,7 @@ export function AttendeeList() {
                 </tbody>
                 <tfoot>
                     <tr>
-                        <TableCell colSpan={3} className='py-3 px-4 text-left text-sm text-zinc-300'>Mostrando 10 de {attendees.length}</TableCell>
+                        <TableCell colSpan={3} className='py-3 px-4 text-left text-sm text-zinc-300'>Mostrando {attendees.length} de {total}</TableCell>
                         <TableCell colSpan={3} className='py-3 px-4 text-right text-sm text-zinc-300'>
                             <div className='inline-flex items-center gap-8'>
                                 <span>Página {page} de {totalPages}</span>
